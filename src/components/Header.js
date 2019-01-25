0ff29e733f2010b3a63633be464ef8d1d38b785e
Dat Nguyen <dat_nguyen@hotmail.co.uk>
2019-01-25 01:17:05 +0000

    utilities and nav menu #2 commit

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import {NavLink } from 'react-router-dom'
+
+import '../styles/Header.scss'
+
+class Header extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = ({ name }) => this.setState({ activeItem: name })
+
+
+  render() {
+    const { activeItem } = this.state
+      
+    return (
+      <div className="ui secondary pointing menu u-margin-bottom-sm">
+        <NavLink  
+          to="/" 
+          className="item"
+          exact
+          activeclassName='active'
+        >
+          Home
+        </NavLink >
+        <NavLink  to="/addRecipe" className="item" activeclassName='active'>
+          Add Recipe
+        </NavLink >
+        <div className="right menu">
+          <button className="ui item">
+            Logout
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header
