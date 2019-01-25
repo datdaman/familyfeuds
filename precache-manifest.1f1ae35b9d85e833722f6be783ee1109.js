23993c6ca921802940812293449e7ff0bf0cc170
Dat Nguyen <dat_nguyen@hotmail.co.uk>
2019-01-25 11:03:34 +0000

    Updates

---
+++ precache-manifest.1f1ae35b9d85e833722f6be783ee1109.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0ab9998c17b87e967a7",
+    "url": "/static/js/main.d0ab9998.chunk.js"
+  },
+  {
+    "revision": "50297478df59f46e684d",
+    "url": "/static/js/1.50297478.chunk.js"
+  },
+  {
+    "revision": "d0ab9998c17b87e967a7",
+    "url": "/static/css/main.5d173d16.chunk.css"
+  },
+  {
+    "revision": "8ba54f569b5fd285b8df4309aca82604",
+    "url": "/index.html"
+  }
+];
