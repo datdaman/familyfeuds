3df371ee13755f17bdd4b1bbb683e5b90ed7c8bf
Dat Nguyen <dat_nguyen@hotmail.co.uk>
2019-01-25 11:09:42 +0000

    Updates

---
+++ precache-manifest.a9d080da11ca9eab22adbf1a678a376f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0619cf5464d560075866",
+    "url": "/familyfeuds/static/js/runtime~main.0619cf54.js"
+  },
+  {
+    "revision": "d0ab9998c17b87e967a7",
+    "url": "/familyfeuds/static/js/main.d0ab9998.chunk.js"
+  },
+  {
+    "revision": "50297478df59f46e684d",
+    "url": "/familyfeuds/static/js/1.50297478.chunk.js"
+  },
+  {
+    "revision": "d0ab9998c17b87e967a7",
+    "url": "/familyfeuds/static/css/main.5d173d16.chunk.css"
+  },
+  {
+    "revision": "3628d83c4b2b44406b3afaec7c2995e5",
+    "url": "/familyfeuds/index.html"
+  }
+];
